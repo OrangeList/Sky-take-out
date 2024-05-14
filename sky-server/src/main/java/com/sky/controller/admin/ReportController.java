679c41c0bba53d3774c35280673524cd1aaf33f7
Tianshizhici <3189753307@qq.com>
2024-05-14 22:41:51 +0800

    苍穹外卖导出运营数据代码

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 /**
@@ -47,6 +48,7 @@ public class ReportController {
 
     /**
      * 用户数据统计
+     *
      * @param begin
      * @param end
      * @return
@@ -62,6 +64,7 @@ public class ReportController {
 
     /**
      * 订单数据统计
+     *
      * @param begin
      * @param end
      * @return
@@ -77,6 +80,7 @@ public class ReportController {
 
     /**
      * 订单数据统计
+     *
      * @param begin
      * @param end
      * @return
@@ -88,5 +92,15 @@ public class ReportController {
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
         log.info("销量排名top10：{}，{}", begin, end);
         return Result.success(reportService.getSaleTop10(begin, end));
+    }
+
+    /**
+     * 导出运营数据报表
+     * @param response
+     */
+    @GetMapping("/export")
+    @ApiOperation("导出运营数据报表")
+    public void export(HttpServletResponse response) {
+        reportService.exportBusinessData(response);
     }
 }
--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -5,17 +5,22 @@ import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
-import com.sky.vo.OrderReportVO;
-import com.sky.vo.SalesTop10ReportVO;
-import com.sky.vo.TurnoverReportVO;
-import com.sky.vo.UserReportVO;
+import com.sky.service.WorkspaceService;
+import com.sky.vo.*;
 import io.swagger.models.auth.In;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -34,6 +39,9 @@ public class ReportServiceImpl implements ReportService {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private WorkspaceService workspaceService;
 
     /**
      * 营业额统计
@@ -220,15 +228,89 @@ public class ReportServiceImpl implements ReportService {
         List<GoodsSalesDTO> salesTop10 = orderMapper.getSalesTop10(beginTime, endTime);
 
         List<String> names = salesTop10.stream().map(GoodsSalesDTO::getName).collect(Collectors.toList());
-        String nameList = StringUtils.join(names,",");
+        String nameList = StringUtils.join(names, ",");
 
         List<Integer> numbers = salesTop10.stream().map(GoodsSalesDTO::getNumber).collect(Collectors.toList());
-        String numberList = StringUtils.join(numbers,",");
+        String numberList = StringUtils.join(numbers, ",");
 
         return SalesTop10ReportVO.builder()
                 .nameList(nameList)
                 .numberList(numberList)
                 .build();
+    }
+
+    /**
+     * 导出运营数据报表
+     *
+     * @param response
+     */
+    public void exportBusinessData(HttpServletResponse response) {
+
+        //查询数据库，获取营业数据
+        LocalDate dateBegin = LocalDate.now().minusDays(30);
+        LocalDate dateEnd = LocalDate.now().minusDays(1);
+
+        //查询概览数据
+        BusinessDataVO businessData = workspaceService.getBusinessData(
+                LocalDateTime.of(dateBegin, LocalTime.MIN),
+                LocalDateTime.of(dateEnd, LocalTime.MAX));
+
+        //通过POI将数据写入到Excel文件当中
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream("template/运营数据报表模板.xlsx");
+
+        try {
+            //基于模板文件创建一个新的Excel文件
+            XSSFWorkbook excel = new XSSFWorkbook(is);
+
+            //获取页
+            XSSFSheet sheet = excel.getSheet("Sheet1");
+
+            //填充数据-时间
+            sheet.getRow(1).getCell(1).setCellValue("时间" + dateBegin + "至" + dateEnd);
+
+            //填充数据-营业额
+            sheet.getRow(3).getCell(2).setCellValue(businessData.getTurnover());
+
+            //填充数据-完成率
+            sheet.getRow(3).getCell(4).setCellValue(businessData.getOrderCompletionRate());
+
+            //填充数据-新增用户数
+            sheet.getRow(3).getCell(6).setCellValue(businessData.getNewUsers());
+
+            //填充数据-有效订单
+            sheet.getRow(4).getCell(2).setCellValue(businessData.getValidOrderCount());
+
+            //填充数据-平均客单价
+            sheet.getRow(4).getCell(4).setCellValue(businessData.getUnitPrice());
+
+            //填充明细数据
+            for (int i = 0; i < 30; i++) {
+                LocalDate date = dateBegin.plusDays(i);
+                BusinessDataVO data = workspaceService.getBusinessData(
+                        LocalDateTime.of(date, LocalTime.MIN),
+                        LocalDateTime.of(date, LocalTime.MAX));
+
+                //获得某一行
+                XSSFRow row = sheet.getRow(i + 7);
+                row.getCell(1).setCellValue(date.toString());
+                row.getCell(2).setCellValue(data.getTurnover());
+                row.getCell(3).setCellValue(data.getValidOrderCount());
+                row.getCell(4).setCellValue(data.getOrderCompletionRate());
+                row.getCell(5).setCellValue(data.getUnitPrice());
+                row.getCell(6).setCellValue(data.getNewUsers());
+            }
+
+            //通过输出流将Excel文件下载到客户端浏览器当中
+            ServletOutputStream outputStream = response.getOutputStream();
+            excel.write(outputStream);
+
+            //关闭资源
+            outputStream.close();
+            excel.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private Integer getOrderCount(LocalDateTime begin, LocalDateTime end, Integer status) {
